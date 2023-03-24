2923b3625481966c48762873d4b54be5908459b7
benoit pype <benoit.pype@gmail.com>
2023-03-24 11:43:06 +0100

    taxifare

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let taxiFareApiUrl = 'http://localhost:8001/predict'; // replace with your API endpoint
+let taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict'; // replace with your API endpoint
 const centralCoordinates = [-74.00597, 40.71427]; // starting position [lng, lat]
 
 if (window.location.href.includes('https://taxifare.lewagon.com')) {
   taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
 }
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
+mapboxgl.accessToken = 'pk.eyJ1IjoiYmVuaXRvLXAiLCJhIjoiY2xmbWR1Y3JtMGF6bzQycjJweDd5N3NveiJ9.9Y5JPdIwIwCYVq4klLi85g';
 
 const displayMap = (start, stop) => {
   const map = new mapboxgl.Map({
@@ -219,6 +219,10 @@ const predict = () => {
     });
   }
 };
+
+
+
+
 
 displayMap();
 pickupAutocomplete();
